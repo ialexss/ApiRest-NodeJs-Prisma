703a11da196bf8aa5cb82f981229ae82c2994661
ialexss <alexandersanchezsalces@gmail.com>
2023-08-17 10:46:42 -0400

    validated controllers

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -18,22 +18,51 @@ const getOneCategory =  async (req, res) => {
 }
 
 const createNewCategory =  async (req, res)=>{
-    const newCategory = await categoriesService.createNewCategory(req.body)
 
-    res.status(200).json(newCategory)
+    const { body } = req;
+
+    if (!body.name) 
+    {
+        const errorMessage = "Campos inválidos en la solicitud.";
+        res.status(400).send(errorMessage); 
+        return; 
+    }
+
+    const newCategory = {
+        name: body.name
+    }
+
+    const createdCategory = await categoriesService.createNewCategory(newCategory)
+
+    res.status(201).json(createdCategory)
 }
 
 const deleteCategory=  async (req, res) => {
-    const categoryDeleted = await categoriesService.deleteCategory(req.params.id)
+
+    const { id } = req.params;
+
+    if(!id)
+        return
+
+    const categoryDeleted = await categoriesService.deleteCategory(id)
 
     if(!categoryDeleted)
         return res.status(404).json({error: "category not found"})
 
-    return res.status(200).json(categoryDeleted)
+    return res.status(204).json(categoryDeleted)
 }
 
 const updateCategory =  async (req,res) =>{
-    const categoryUpdate = await categoriesService.updateCategory(req.params.id,req.body)
+
+    const { body, params: { id } } = req;
+
+    if(!id)
+        return
+
+    const categoryUpdate = await categoriesService.updateCategory(id,body)
+
+    if(!categoryUpdate)
+        return res.status(404).json({error: "category not found"})
 
     return res.status(200).json(categoryUpdate)
 }
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -18,22 +18,58 @@ const getOneProduct =  async (req, res) => {
 }
 
 const createNewProduct =  async (req, res)=>{
-    const newProduct = await productsService.createNewProduct(req.body)
 
-    res.status(200).json(newProduct)
+    const { body } = req;
+
+    if (
+        !body.name ||
+        !body.price || typeof body.price !== 'number' ||
+        !body.categoryId ||
+        !body.stock || typeof body.stock !== 'number'
+    ) {
+        const errorMessage = "Campos inválidos en la solicitud.";
+        res.status(400).send(errorMessage); 
+        return; 
+    }
+
+    const newProduct = {
+        name: body.name,
+        price: body.price,
+        categoryId: body.categoryId,
+        stock: body.stock
+    }
+
+    const createdProduct = await productsService.createNewProduct(newProduct)
+
+    res.status(201).json(createdProduct)
 }
 
 const deleteProduct =  async (req, res) => {
-    const productDeleted = await productsService.deleteProduct(req.params.id)
+
+    const { id } = req.params;
+
+    if(!id)
+        return
+
+    const productDeleted = await productsService.deleteProduct(id)
 
     if(!productDeleted)
         return res.status(404).json({error: "Product not found"})
 
-    return res.status(200).json(productDeleted)
+    return res.status(204).json(productDeleted)
 }
 
 const updateProduct =  async (req,res) =>{
-    const productUpdate = await productsService.updateProduct(req.params.id,req.body)
+
+    const { body, params: { id } } = req;
+
+    if(!id)
+        return
+
+    const productUpdate = await productsService.updateProduct(id,body)
+
+    if(!productUpdate)
+        return res.status(404).json({error: "Product not found"})
 
     return res.status(200).json(productUpdate)
 }
